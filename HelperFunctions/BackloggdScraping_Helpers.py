a6c28580a88ed2b26c66fef80f88bf0cbe83041a
Tuhin Patel <tpatel16@wgu.edu>
2026-02-17 16:11:16 -0500

    Update BackloggdScraping_Helpers.py
    Removed the call to perform_random_delay from scrape_backloggd_game_links, as it was unnecessary. Also added a function to scrape a specific backloggd page that will be updated with more helper functions to clean up the data

--- HelperFunctions/BackloggdScraping_Helpers.py
+++ HelperFunctions/BackloggdScraping_Helpers.py
@@ -32,7 +32,38 @@ def scrape_backloggd_game_links(page_link: str, headers=headers):
     game_links = []
     for game in games_on_page:
         game_links.append("https://backloggd.com" + game["href"])
-        perform_random_delay()
     
     # Return the final array
     return game_links
+
+# Extract all needed information from one backloggd page
+def scrape_backloggd_page(page_link:str, headers=headers):
+    
+    # Get the page's contents
+    req = requests.get(page_link, headers=headers)
+    if req.status_code != 200:
+        print("FAILED")
+        return {}
+    
+    soup = BeautifulSoup(req.text, 'html.parser')
+    
+    # Get the average rating
+    avg_rating =  soup.find("div", class_ ="container backloggd-container center-container h-100").find("h1").text
+
+    # Get the number of ratings
+    num_ratings = soup.find("div", class_ = "backloggd-container center-container log-counters h-100").find_all("p", class_ = "mb-0 log-counter-stat")[4].text
+
+    # Get the average playimr
+    avg_playtime = soup.find("div", class_ = "backloggd-container center-container theme-pink sm-container h-100").find("p", "mb-0 stat-value element-revealed").text
+
+    # Get the release date
+    release_date = soup.find("div", class_ = "container backloggd-container sm-container").find("a").text
+
+    # Platforms can include multiple consoles, so we need to iterate through all the results
+    platforms_html = soup.find("div", class_ = "col ml-auto text-right text-md-left").find_all("a")
+    platforms = []
+    for item in platforms_html:
+        platforms.append(item.text)
+
+    return {'Average Backloggd Rating': avg_rating, 'Number of Backloggd Ratings': num_ratings, 'Average Backloggd Playime':avg_playtime,
+            'Release Date': release_date, 'Platforms': platforms}
